768b7e3d23af56cf4ad7d8849917d66f161ff00a
Alex <Zerfalex@gmail.com>
2019-05-08 17:08:48 +0100

    Update ag.c

--- ag.c
+++ ag.c
@@ -21,12 +21,13 @@ typedef void(*sighandler_t)(int);
 #define QTD 3
 #define LINE ID + 1 + PRICE + 1 + QTD + 1
 
-/*
-	printf("[%s] [%s] [%s]\n",inString[0], inString[1], inString[2]);
-	
-	printf("%d %d %d\n",inInt[0],inInt[1], inInt[2]);
-*/
+void freeC(char** c){
+	free(c[0]);
+	free(c[1]);
+	free(c[2]);
+}
 
+//Reads a line in the format, storing the values in the given params
 int readLnC(int fd, char** inS, int* in) {	
 	
 	read(fd, inS[0], ID);
@@ -43,31 +44,7 @@ int readLnC(int fd, char** inS, int* in) {
 	return r;
 }
 
-int readLn(int fd, int* in) {	
-
-	char** inS = malloc(sizeof(char*)*3);
-	inS[0] = malloc(sizeof(char) * ID);
-	inS[1] = malloc(sizeof(char) * PRICE);
-	inS[2] = malloc(sizeof(char) * QTD);
-
-	read(fd, inS[0], ID);
-	lseek(fd, 1, SEEK_CUR);
-	read(fd, inS[1], PRICE);
-	lseek(fd, 1, SEEK_CUR);
-	int r = read(fd, inS[2], QTD);
-	lseek(fd, 1, SEEK_CUR);
-
-	in[0] = atoi(inS[0]);
-	in[1] = atoi(inS[1]);
-	in[2] = atoi(inS[2]);
-	//free(inS[0]);
-	//free(inS[1]);
-	//free(inS[2]);
-	//free(inS);
-
-	return r;
-}
-
+//Returns the file position of the beggining of the line with id;
 off_t getPos(int fd, char* id) {
 
 	off_t end = lseek(fd, 0L, SEEK_END);
@@ -88,24 +65,16 @@ off_t getPos(int fd, char* id) {
 	lseek(fd,0,SEEK_SET);
 }
 
-void test(FILE* f, off_t pos) {
-	
-	char c[16] = "hello i am gay\n";
-	fseek(f,pos,SEEK_SET);
-	
-	fwrite(c,1,15,f);
-}
-
-void ftest(int fd, FILE* f, off_t pos, int* in, char** s) {
+//Replaces the values at pos with the ones at in using s as a string buffer;
+void fReplace(int fd, FILE* f, off_t pos, int* in, char** s) {
 	
 	lseek(fd,pos,SEEK_SET);
 	int old[3];
 	old[1] = 0;
 	old[2] = 0;
-	readLn(fd, old);
-	
-	printf("hm-> %d + %d = %d\n",in[1], old[1], in[1] + old[1]);
-	printf("hm-> %d + %d = %d\n",in[2], old[2], in[2] + old[2]);
+	sprintf(s[1],"%0*d", PRICE, 0);
+	sprintf(s[2],"%0*d", QTD, 0);
+	readLnC(fd, s, old);
 	
 	in[1] += old[1];
 	in[2] += old[2];
@@ -113,8 +82,6 @@ void ftest(int fd, FILE* f, off_t pos, int* in, char** s) {
 	char c[LINE + 1];
 	sprintf(s[1],"%0*d", PRICE, in[1]);
 	sprintf(s[2],"%0*d", QTD, in[2]);
-
-	printf("hm-> %s %s\n",s[1], s[2]);
 	
 	fseek(f,pos,SEEK_SET);
 	fwrite(s[0],1,ID,f);
@@ -126,6 +93,7 @@ void ftest(int fd, FILE* f, off_t pos, int* in, char** s) {
 	
 }
 
+//Reads each line from stdin and for each one searches for its values in the tmp file inserting them;
 int main(int argc, char* argv[]) {
 	//sprintf(buf,"%0*d\n", ID, 36182);
 	int fd = open("tmp", O_CREAT | O_RDWR, 0666);
@@ -144,9 +112,7 @@ int main(int argc, char* argv[]) {
 	int i = 0;
 	while (readLnC(0, inS, intNew) > 0) {
 		int pos = getPos(fd,inS[0]);
-		printf("%d\n",pos);
-		
-		ftest(fd, f,pos, intNew, inS);
+		fReplace(fd, f,pos, intNew, inS);
 		fseek(f,0,SEEK_END);
 		i++;
 	}
@@ -159,6 +125,7 @@ int main(int argc, char* argv[]) {
 	if (remove("tmp") < 0) {
 		perror("Could not delete file");
 	}
+	freeC(inS);
 	return 1;
 	
 }
